8d7674eb0536f488d00abaa913d85262101cf750
Administrator <Administrator@305-38>
2016-04-25 11:17:57 +0900

    뺄셈 기능과 테스트 코드 추가

--- src/main/java/reviewproj/CalcReview.java
+++ src/main/java/reviewproj/CalcReview.java
@@ -4,8 +4,13 @@ public class CalcReview {
 	private int result=0;
 	
 	public void add(int x, int y) {
-		result=x+y;
+		result = x + y;
 	}
+	
+	public void sub(int x, int y) {
+		result = x - y;
+	}
+	
 	public int getResult() {
 		return result;
 	}
